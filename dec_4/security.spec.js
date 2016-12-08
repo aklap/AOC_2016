81762a728134bddfc0f3332439e1f5131fe0826c
Alexis La Porte <aklaporte@gmail.com>
2016-12-08 00:27:13 -0500

    moved files

--- dec_4/security.spec.js
+++
@@ -1,6 +0,0 @@
-var security = require('./security');
-// describe('check sum total', function(){
-//     it('returns the correct num', function(done) {
-//        // Add async jasmine testing
-//     });
-// });
