e9006daec4944d4cd73e7b9d49b0ee84c698c51f
Alexis La Porte <aklaporte@gmail.com>
2016-12-08 00:22:28 -0500

    move part I into separate dir

---
+++ dec_4/checkSum/security.spec.js
@@ -0,0 +1,7 @@
+var security = require('./security');
+// var decrypt = require('./decrypt');
+// describe('check sum total', function(){
+//     it('returns the correct num', function(done) {
+//        // Add async jasmine testing
+//     });
+// });
