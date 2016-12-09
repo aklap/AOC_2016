8a2af0c1a72706d7db8d583518feee16c9676157
Alexis La Porte <aklaporte@gmail.com>
2016-12-09 00:56:31 -0500

    add inital files and input

---
+++ dec_7/tls.js
@@ -0,0 +1,10 @@
+// IP supports TLS if it has an Autonomous Bridge Bypass Annotation, or ABBA. 
+
+// An ABBA is any four-character sequence which consists of a pair of two different characters followed by the reverse of that pair, such as xyyx or abba. 
+
+// IP also must not have an ABBA within any hypernet sequences, which are contained by square brackets.
+
+
+function hasTLS() {
+
+}
