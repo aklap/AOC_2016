a639c1db18b7c315a8e177adccb7dd039661fb27
Alexis La Porte <aklaporte@gmail.com>
2016-12-08 23:01:01 -0500

    fix formatting

--- dec_6/frequencies.js
+++ dec_6/frequencies.js
@@ -2,17 +2,17 @@ var helpers = require('../dec_4/mostCommon/mostCommonHelpers');
 
 module.exports = {
     getModes: function(cols) {
-    var allModes = [];
+        var allModes = [];
 
-    for(var i=0; i<cols.length; i++) {
-            var modes = {};
-        for(var j=0; j<cols[i].length; j++) {
-            var letter = cols[i][j];
+        for(var i=0; i<cols.length; i++) {
+                var modes = {};
+            for(var j=0; j<cols[i].length; j++) {
+                var letter = cols[i][j];
 
-            !modes[letter] ? modes[letter] = 1 : modes[letter]++;
+                !modes[letter] ? modes[letter] = 1 : modes[letter]++;
+            }
+                allModes.push(helpers.getList(modes));
         }
-            allModes.push(helpers.getList(modes));
-    }
 
         return allModes;
     },
