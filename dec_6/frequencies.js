3dc4e8162dfa3571c58b00a590d4057d5edb56cb
Alexis La Porte <aklaporte@gmail.com>
2016-12-08 21:59:28 -0500

    move freqency related functions to separate file

---
+++ dec_6/frequencies.js
@@ -0,0 +1,27 @@
+var helpers = require('../dec_4/mostCommon/mostCommonHelpers');
+
+module.exports = {
+    getModes: function (cols) {
+    var allModes = [];
+
+    for(var i=0; i<cols.length; i++) {
+            var modes = {};
+        for(var j=0; j<cols[i].length; j++) {
+            var letter = cols[i][j];
+
+            !modes[letter] ? modes[letter] = 1 : modes[letter]++;
+        }
+            allModes.push(helpers.getList(modes));
+    }
+
+        return allModes;
+    },
+
+    mostFrequent: function (c) {
+        return c[0];
+    },
+
+    leastFrequent: function (c) {
+        return c[c.length-1];
+    }
+};
