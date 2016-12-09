978d35ba6667b4193c5e16ae1d62252477c2014e
Alexis La Porte <aklaporte@gmail.com>
2016-12-08 21:59:07 -0500

    refactor, move functions into separate files

--- dec_6/decrypt.js
+++ dec_6/decrypt.js
@@ -1,49 +1,23 @@
 var fs = require('fs');
-var sortDesc = require('../dec_4/mostCommon/mostCommonHelpers').sortDesc;
-var getList = require('../dec_4/mostCommon/mostCommonHelpers').getList;
 
+// functions
+var helpers = require('../dec_4/mostCommon/mostCommonHelpers'),
+    transform = require('./transform.js'),
+    freq = require('./frequencies.js');
 
-var lines = fs.readFileSync('test.txt').toString().split('\n');
-
-var rows = lines.map(function(el, i){
-    return el.trim().split('');
-});
-
-function transpose(rows) {
-    return rows[0].map(function(_, j) {
-        return rows.map(function(_, k) {
-            
-            if(rows[k][j] !== undefined) {
-                return rows[k][j];
-            };
-        }).filter(Boolean);
+var lines = fs.readFileSync('input.txt').toString().split('\n'),
+    rows = transform.getRows(lines),
+    cols = transform.transpose(rows),
+    letters = freq.getModes(cols).map(function(c) {
+        return helpers.sortDesc(c).map(function(pair) {
+            return Object.keys(pair).toString();
+        });
     });
-}
 
-var cols = transpose(rows);
+// solution to part I
+var mostFrequent = letters.map(freq.mostFrequent).join('');
 
-function getModes(cols) {
-    var allModes = [];
+// solution to part II
+var leastFrequent = letters.map(freq.leastFrequent).join('');
 
-    for(var i=0; i<cols.length; i++) {
-            var modes = {};
-        for(var j=0; j<cols[i].length; j++) {
-             if(!modes[cols[i][j]]) {
-                modes[cols[i][j]] = 1;
-            } else {
-                modes[cols[i][j]]++;
-            }
-        }
-            allModes.push(getList(modes));
-    }
-
-    return allModes;
-}
-
-console.log(getModes(cols).map(function(c) {
-    return sortDesc(c).map(function(d) {
-        return Object.keys(d).toString();
-    });
-}).map(function(r, i){
-    return r[0];
-}).join(''));
+console.log(mostFrequent, leastFrequent);
